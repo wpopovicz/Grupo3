17a5d49fcbf6eb3269f97a41000064e2bb4c4709
Lucas Augusto Silverio de Oliveira <lucaum2@hotmail.com>
2013-06-10 22:14:35 -0300

    teste

--- principal/src/principal/Principal.java
+++ principal/src/principal/Principal.java
@@ -14,6 +14,6 @@ public class Principal {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("ola");
+        System.out.println("ola2");
     }
 }
