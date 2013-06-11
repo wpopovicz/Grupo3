6f511dd9033d39918d7efb10dc93a185ac70152c
willian lopes popovicz <popovicz_wil@hotmail.com>
2013-06-10 21:56:21 -0300

    criando o projeto netbeans

---
+++ principal/src/principal/Principal.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package principal;
+
+/**
+ *
+ * @author a1294121
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("ola");
+    }
+}
