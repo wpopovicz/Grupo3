bb52a86ca77cac8a748c00f43bd8730a8c2bce86
wpopovicz <popovicz_wil@hotmail.com>
2013-07-09 15:04:24 -0300

    #1 melhorando

---
+++ Projeto/src/Model/DaoPessoa.java
@@ -0,0 +1,95 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author a1294121
+ */
+public class DaoPessoa implements Dao<Pessoa> {
+
+    private static Pessoa converteRsParaPessoa(ResultSet rs) throws SQLException {
+        Pessoa p = new Pessoa();
+        p.setId(rs.getInt("id"));
+        p.setNome(rs.getString("nome"));
+        p.setSenha(rs.getString("senha"));
+        p.setSaldo(rs.getString("saldo"));
+        p.setEmail(rs.getString("email"));
+        return p;
+    }
+
+    @Override
+    public void persist(Pessoa p) throws SQLException{
+        if(p.getId()==0)
+            insert(p);
+        else
+            update(p);
+    }
+    
+    private static void insert(Pessoa p) throws SQLException{
+        PreparedStatement pst =  Conection.prepareConnection().prepareStatement("INSERT INTO pessoa (nome, senha, saldo, email) VALUES(?,?,?,?)", Statement.RETURN_GENERATED_KEYS);
+        pst.setString(1, p.getNome());
+        pst.setString(2, p.getSenha());
+        pst.setString(3, p.getSaldo());
+        pst.setString(4, p.getEmail());
+        
+        pst.execute();
+        
+        ResultSet rs = pst.getGeneratedKeys();
+        rs.next();
+        p.setId(rs.getInt(1));
+        
+    }
+    
+    @Override
+    public Pessoa retrieve(int id) throws SQLException{
+        Statement st =  Conection.prepareConnection().createStatement();                                
+        st.execute("SELECT * FROM pessoa WHERE id =" + id);
+        ResultSet rs = st.getResultSet();
+        
+        rs.next();
+        Pessoa p = converteRsParaPessoa(rs);
+       
+        return p;
+    }
+
+    private static void update(Pessoa p) throws SQLException{
+        PreparedStatement pst =  Conection.prepareConnection().prepareStatement("UPDATE pessoa SET nome = ?, Senha = ?, saldo = ?, email = ? WHERE id = ?");
+        pst.setString(1, p.getNome());
+        pst.setString(2,p.getSenha());
+        pst.setString(3, p.getSaldo());
+        pst.setString(4, p.getEmail());
+        pst.setInt(5, p.getId());
+        pst.execute();
+    }
+    
+    @Override
+    public void delete(Pessoa p) throws SQLException{
+        Statement st =  Conection.prepareConnection().createStatement();                                
+        st.execute("DELETE FROM pessoa WHERE id = " + p.getId());
+    }
+    
+    @Override
+    public List<Pessoa> list() throws SQLException{
+        List<Pessoa> Pessoas = new ArrayList<Pessoa>();
+        
+        Statement st =  Conection.prepareConnection().createStatement();                                
+        ResultSet rs =  st.executeQuery("SELECT * FROM pessoa");
+        
+        while(rs.next()){
+            Pessoa p = converteRsParaPessoa(rs);
+            Pessoas.add(p);
+        }
+        
+        return Pessoas;
+    }
+}
---
+++ Projeto/src/Model/Pessoa.java
@@ -0,0 +1,74 @@
+  /*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author a1294121
+ */
+public class Pessoa {
+
+    private int id;
+    private String nome;
+    private String Senha;
+    private String Saldo;
+    private String Email;
+
+    public Pessoa() {
+    }
+
+    public Pessoa(int id, String nome, String Senha, String Saldo, String Email) {
+        this.id = id;
+        this.nome = nome;
+        this.Senha = Senha;
+        this.Saldo = Saldo;
+        this.Email = Email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSenha() {
+        return Senha;
+    }
+
+    public void setSenha(String Senha) {
+        this.Senha = Senha;
+    }
+
+    public String getSaldo() {
+        return Saldo;
+    }
+
+    public void setSaldo(String Saldo) {
+        this.Saldo = Saldo;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" + "id=" + id + ", nome=" + nome + ", Senha=" + Senha + ", Saldo=" + Saldo + ", Email=" + Email + '}';
+    }
+}
